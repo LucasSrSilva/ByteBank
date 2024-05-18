aa61a19509bd1f290a3a1a2ca59f104b252cab09
LucasSrSilva <lucas.srs.2022@gmail.com>
2024-05-18 10:48:53 -0300

    first commit

---
+++ src/components/data-component.ts
@@ -0,0 +1,19 @@
+import { FormatoData } from "../types/FormatoData.js";
+import Conta from "../types/conta.js";
+import { formatarData } from "../utils/formatters.js";
+
+const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
+
+function rederizarData(): void {
+    if(elementoDataAcesso != null){
+        elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
+    }
+}
+
+const DataComponent = {
+    atualizar(){
+        rederizarData();
+    }
+}
+
+export default DataComponent;
